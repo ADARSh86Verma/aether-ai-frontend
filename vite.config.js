5e0b3fa725179471b2e8169e4d3d3edaca23a897
ADARSh86Verma <vermaaadarsh867481@gmail.com>
2026-08-21 05:19:40 +0530

    Update API destination URLs in Vercel and Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
 
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: " https://wall-broadcast-launches-emotional.trycloudflare.com",
         changeOrigin: true,
         rewrite: (path) =>
           path.replace(/^\/api/, ""),
